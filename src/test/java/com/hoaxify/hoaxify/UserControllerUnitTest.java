3c956463061ade56cf5d874d1fbc9c1cf1eb51b2
Ben WU <wubinben@icloud.com>
2022-12-31 23:06:02 +0800

    test: UserControllerUnitTest.postUser_whenUserIsValid_passwordIsHashedInDatabase()

--- src/test/java/com/hoaxify/hoaxify/UserControllerUnitTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerUnitTest.java
@@ -1,6 +1,8 @@
 package com.hoaxify.hoaxify;
 
+import com.hoaxify.hoaxify.user.User;
 import com.hoaxify.hoaxify.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -21,6 +25,11 @@ class UserControllerUnitTest {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@BeforeEach
+	public void cleanUp() {
+		userRepository.deleteAll();
+	}
 
 	@Test
 	void postUser_whenUserIsValid_userSavedToDatabase() throws Exception {
@@ -39,4 +48,22 @@ class UserControllerUnitTest {
 		assertThat(userRepository.findByUsername("ben").get(0).getDisplayName()).isEqualTo("Ben");
 	}
 
+	@Test
+	void postUser_whenUserIsValid_passwordIsHashedInDatabase() throws Exception {
+		String newUserInJson = """
+                        {
+                        "username": "ben",
+                        "displayName": "Ben",
+                        "password": "P4ssword"
+                        }
+				""";
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/1.0/users")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(newUserInJson))
+				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.message").value("user saved"));
+		List<User> users = userRepository.findAll();
+		User user = users.get(0);
+		assertThat(user.getPassword()).isNotEqualTo("P4ssword");
+	}
 }
